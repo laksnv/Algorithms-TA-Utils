af05c70a5a45da2bf55bec0a4e47216bb3165498
Lakshmi Narasiman <vigneshnarasiman@gmail.com>
2015-05-11 14:04:20 -0400

    Extract .tar.gz files in their own directory

--- unzip-recur.py
+++ unzip-recur.py
@@ -1,3 +1,4 @@
 import os
 
 os.system("find . -name \"*.zip\" | while read filename; do unzip -o -d \"`dirname \"$filename\"`\" \"$filename\"; done;")
+os.system("find . -name \"*.tar.gz\" | while read filename; do tar -xvzf \"$filename\" -C \"`dirname \"$filename\"`\"; done;")
